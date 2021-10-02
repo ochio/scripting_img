360c938696fc7f6826adac1c5798fc2ef514af2c
ochi <ocos54ce@gmail.com>
2021-10-03 00:00:07 +0900

    feat: handle error

--- main.ts
+++ main.ts
@@ -4,13 +4,15 @@ import https from 'https'
 for(let i = 0; i < 100; i++){
 	const id = Number(process.env.BASE) + i
 	const url = process.env.URL +`fd${id}.jpg`
-	console.log(id,url);
-	const file = fs.createWriteStream(`./img/${id}.jpg`)
 	const req = https.get(url, (res) => {
-		res.pipe(file)
-		res.on('end', ()=>{
-			file.close()
-		})
+		if(res.statusCode !== 404){
+			const file = fs.createWriteStream(`./img/${id}.jpg`)
+			res.pipe(file)
+			res.on('end', ()=>{
+				console.log(`add ${url}`);
+				file.close()
+			})
+		}
 	})
 	req.on('error', (err) => {
 		console.log(id,err);
